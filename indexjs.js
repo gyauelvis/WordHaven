c3cc6a79cf3aee4b4e2161531662e884ce88567d
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-01-03 22:38:04 +0000

    I worked on the Sermon aspect of the home page
    I added a few new features and delected the youtube part

--- indexjs.js
+++ indexjs.js
@@ -2,6 +2,8 @@ let hamburger = document.querySelector(".navBar .theBox .hamburger");
 let navLinks = document.querySelector(".navBar .navLinks");
 let closeMenu = document.querySelector(".navBar .closeMenu img");
 let navLinksUl = document.querySelector(".navBar .navLinks ul");
+let getSermonImg = document.querySelectorAll(".sermons .serMon .sermonImg");
+let getPlayIcon = document.querySelectorAll(".sermons .serMon .sermonImg img");
 const toggleOn = (e) => {
   e.preventDefault;
   navLinksUl.style.animation = "ul-in 250ms ease-in-out 0s 1 normal forwards";
@@ -40,3 +42,12 @@ values.forEach(function (value) {
 
 let pic_anim = document.querySelectorAll(".pic_anim");
 pic_anim.forEach((pic) => observer.observe(pic));
+
+// const hoverOnSermons = () => {
+//   getPlayIcon.forEach((icon) => {
+//     icon.style.backgroundColor = "#fff";
+//   });
+// };
+getSermonImg.forEach((sermonImg) => {
+  sermonImg.addEventListener("mouseover", hoverOnSermons);
+});
