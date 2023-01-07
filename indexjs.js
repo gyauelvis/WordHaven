ecaea041bbbf60ade0f60d293ef170b273290272
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-01-07 21:02:27 +0000

    7th Jan Changes

--- indexjs.js
+++ indexjs.js
@@ -2,8 +2,6 @@ let hamburger = document.querySelector(".navBar .theBox .hamburger");
 let navLinks = document.querySelector(".navBar .navLinks");
 let closeMenu = document.querySelector(".navBar .closeMenu img");
 let navLinksUl = document.querySelector(".navBar .navLinks ul");
-let getSermonImg = document.querySelectorAll(".sermons .serMon .sermonImg");
-let getPlayIcon = document.querySelectorAll(".sermons .serMon .sermonImg img");
 const toggleOn = (e) => {
   e.preventDefault;
   navLinksUl.style.animation = "ul-in 250ms ease-in-out 0s 1 normal forwards";
@@ -36,18 +34,23 @@ observer = new IntersectionObserver(function (entries) {
     }
   });
 });
-values.forEach(function (value) {
+values.forEach((value) => {
   observer.observe(value);
 });
 
 let pic_anim = document.querySelectorAll(".pic_anim");
 pic_anim.forEach((pic) => observer.observe(pic));
 
-// const hoverOnSermons = () => {
-//   getPlayIcon.forEach((icon) => {
-//     icon.style.backgroundColor = "#fff";
-//   });
-// };
-getSermonImg.forEach((sermonImg) => {
-  sermonImg.addEventListener("mouseover", hoverOnSermons);
+let sermons = document.querySelectorAll(".sermonImg");
+sermons.forEach((sermon) => {
+  sermon.addEventListener("mouseover", (e) => {
+    e.target.firstElementChild.style.backgroundColor = "#58181f";
+  });
+});
+sermons = document.querySelectorAll(".sermonImg");
+
+sermons.forEach((sermon) => {
+  sermon.addEventListener("mouseout", (e) => {
+    e.target.firstElementChild.style.backgroundColor = "";
+  });
 });
