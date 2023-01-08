2bd69ff8c9b735c6b63291c01acbc8b8201c1740
Gyau Boahen Elvis <66832781+gyauelvis@users.noreply.github.com>
2023-01-08 06:52:39 +0000

    Learnt more about intersection observer and applied it here

--- indexjs.js
+++ indexjs.js
@@ -19,7 +19,14 @@ closeMenu.addEventListener("click", toggleOff);
 
 // The stugger animation of the church values
 let values = document.querySelectorAll(".value");
-observer = new IntersectionObserver(function (entries) {
+const observer = new IntersectionObserver(function (entries) {
+  // So as to tell the browser to use its view port to tract the intersectioon of the element
+  root: null;
+  // Defines set of ratios for which the intersection observer should intersect
+  // 1.0 means the entire element has to be intersected first
+  thresholds: [1.0];
+  // Defines margins around the root so that it does not observe untill it has gotten there yet
+  rootMargin: "10rem 0 10rem 0";
   entries.forEach(function (entry) {
     if (entry.isIntersecting) {
       entry.target.style.opacity = "1";
@@ -28,7 +35,7 @@ observer = new IntersectionObserver(function (entries) {
       entry.target.style.transition = `all 500ms ease-in-out ${entry.target.dataset.delay}`;
     } else {
       entry.target.style.opacity = "0";
-      entry.target.style.transform = "translateY(50%)";
+      entry.target.style.transform = "translateY(40%)";
       entry.target.style.filter = "blur(5px)";
       entry.target.style.transition = `all 500ms ease-in-out ${entry.target.dataset.delay}`;
     }
